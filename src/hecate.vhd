548e8cdbc41e52b13366016f7af090166e750a26
RicardoLera <ricardodclera@gmail.com>
2024-05-10 16:25:05 -0300

    Finished except for FFT order thing

--- src/hecate.vhd
+++ src/hecate.vhd
@@ -31,7 +31,7 @@ architecture arch of hecate is
   signal ready_idft : std_logic_vector(0 to 26) := (others => '0');
   signal dfts_ready, hads_ready, idfts_ready, s_ready : std_logic := '0';
 
-  signal add_a, add_b, add_r : b25_complex_array(0 to 26) := (others => (others => (others => '0')));
+  signal add_a, add_b, add_r : b25_real_array(0 to 26) := (others => (others => '0'));
   signal acc : b25_real_array(0 to 26) := (others => (others => '0'));
   
 begin
@@ -110,28 +110,21 @@ begin
 
     sum_r : component b25_add
       port map (
-        a   => add_a(o_id)(0),
-        b   => add_b(o_id)(0),
-        res => add_r(o_id)(0)
-      );
-
-    sum_i : component b25_add   -- for debugging
-      port map (
-        a   => add_a(o_id)(1),
-        b   => add_b(o_id)(1),
-        res => add_r(o_id)(1)
+        a   => add_a(o_id),
+        b   => add_b(o_id),
+        res => add_r(o_id)
       );
     
     sum_out : component b25_add
       port map (
-        a   => add_r(o_id)(0),
+        a   => add_r(o_id),
         b   => acc(o_id),
         res => res(o_id)
       );
 
     sum_pro : process (clock) is
 
-      variable i_id, w_ex, w_exc, w, wc: natural range 0 to 32 := 0;
+      variable i_id, w_ex, w_exc, w, wc: natural range 0 to 32;
       variable i_id_cor : natural range 0 to 16;
       variable a_sign, b_sign, wx_sign, wy_sign : std_logic;
 
@@ -150,10 +143,11 @@ begin
             end if;
 
             -- Exponent of w in full unit circle
-            w_ex := (i_id_cor * o_id) mod 32;
+            w_ex := (i_id * o_id) mod 32;
+            -- report "o_id = " & to_string(o_id) & "   i = " & to_string(i_id) & "   w_ex = " & to_string(w_ex);
             
             -- Second and fourth quadrant reflections
-            if ((w_ex > 8) and (w_ex <= 16)) or (w_ex > 24 and w_ex <= 31) then
+            if (((w_ex > 8) and (w_ex <= 16)) or ((w_ex > 24) and (w_ex <= 31))) then
               w := (8 - (w_ex mod 8)) mod 8;
             else
               w := w_ex mod 8;
@@ -174,27 +168,21 @@ begin
             wx_sign := '1' when cos_sig_ref(w_ex)=1 else '0';
             wy_sign := '1' when sin_sig_ref(w_ex)=1 else '0';
 
-            add_a(o_id)(0) <= calc_vals_x(i_id_cor)(w);  -- a_wx
-            add_a(o_id)(1) <= calc_vals_x(i_id_cor)(wc); -- a_wy
-            add_b(o_id)(1) <= calc_vals_y(i_id_cor)(w);  -- b_wx
-            add_b(o_id)(0) <= calc_vals_y(i_id_cor)(wc); -- b_wy
+            add_a(o_id) <= calc_vals_x(i_id_cor)(w);  -- a_wx
+            add_b(o_id) <= calc_vals_y(i_id_cor)(wc); -- b_wy
 
-            if (i_id > 16) then b_sign := not b_sign; end if; -- Complex conjugate inversion (b)
+            --if (i_id > 16) then b_sign := not b_sign; end if; -- Complex conjugate inversion (b)
             wy_sign := not wy_sign;                           -- DFT/IDFT inversion
 
-            add_a(o_id)(0)(24) <= a_sign xor wx_sign;
-            add_a(o_id)(1)(24) <= a_sign xor wy_sign;
-            add_b(o_id)(1)(24) <= b_sign xor wx_sign;
-            add_b(o_id)(0)(24) <= not (b_sign xor wy_sign);   -- i^2 inversion
+            add_a(o_id)(24) <= a_sign xor wx_sign;
+            add_b(o_id)(24) <= not (b_sign xor wy_sign);   -- i^2 inversion
 
             -- Orthogonal cancellations -> something is still leaking through, making this necessary
             if (w_ex=8 or w_ex=24) then -- cancel wx
-              add_a(o_id)(0) <= (others => '0');
-              add_b(o_id)(1) <= (others => '0');
+              add_a(o_id) <= (others => '0');
             end if;
             if (w_ex=0 or w_ex=16) then -- cancel wy
-              add_a(o_id)(1) <= (others => '0');
-              add_b(o_id)(0) <= (others => '0');
+              add_b(o_id) <= (others => '0');
             end if;
             
             acc(o_id) <= res(o_id);
