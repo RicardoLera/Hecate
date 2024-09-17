0e4cded564596b233302f517504701e9df53ca81
RicardoLera <ricardodclera@gmail.com>
2024-09-17 13:56:07 -0300

    fft sequentiality

--- src/fft/fft.vhd
+++ src/fft/fft.vhd
@@ -37,6 +37,10 @@ architecture synth of fft is
   signal wmul_in_d, wmul_out_d : b25_complex_array(0 to n_points/4-2);
   signal wmul_in_s, wmul_out_s : b25_complex_array(0 to 1);
 
+  signal fly_done : std_logic_vector (0 to n_points/2-1) := (others => '0');
+  signal mul_done : std_logic_vector (0 to n_points/4-2) := (others => '0');
+  signal fly_done_all, mul_done_all : std_logic := '0';
+
   signal out_buff  : b25_complex_array(0 to n_points/2) := (others => (others => (others => '0')));
 
   function FFT_scramble(x, y, z : integer) return integer is
@@ -55,31 +59,6 @@ architecture synth of fft is
 
 begin
 
-  -- State machine
-  state_machine : process (clock) begin
-    if rising_edge(clock) then
-
-      if (reset) then
-        state <= (others => '0');
-        s_ready <= '0';
-        for b in 0 to n_points-1 loop
-          fly_raster(b) <= (in_raster(b), 25b"0");
-        end loop;
-
-      elsif (start) then
-        if (state < cycles-1) then
-          for b in 0 to n_points-1 loop
-            fly_raster(b) <= buf_raster(b);
-          end loop;
-          state <= state + 1;
-        else
-          s_ready <= '1';
-        end if;
-      end if;
-      
-    end if;
-  end process state_machine;
-
   -- Rasterize and scramble
   gen_x : for x in 0 to nx-1 generate
     gen_y : for y in 0 to ny-1 generate
@@ -89,7 +68,70 @@ begin
     end generate gen_y;
   end generate gen_x;
 
-  -- Generate complex constant multiplers
+  -- State machine
+  state_machine : process (clock) begin
+    if rising_edge(clock) then
+
+      if (reset) then
+        state <= (others => '0');
+        s_ready <= '0';
+        fly_done <= (others => '0');
+        mul_done <= (others => '0');
+        for b in 0 to n_points-1 loop
+          fly_raster(b) <= (in_raster(b), 25b"0");
+        end loop;
+
+      elsif (start) then
+        if (state < cycles-1) then
+          fly_done <= (others => '0');
+          mul_done <= (others => '0');
+          state <= state + 1;
+        else
+          s_ready <= '1';
+        end if;
+      end if;
+      
+    end if;
+  end process state_machine;
+
+  -- Generate butterflies and their behavior
+  gen_bfly : for b in 0 to n_points/2-1 generate
+
+    wmul_wsym : component b25_butterfly
+      port map (
+        i_top => bfly_in_top(b),
+        i_bot => bfly_in_bot(b),
+        o_top => bfly_out_top(b),
+        o_bot => bfly_out_bot(b)
+      );
+
+    proc_fly : process (state) is
+      constant b_size     : integer := 2**to_integer(state) + 1;
+      constant group_size : integer := 2**to_integer(state);
+      constant group_idx  : integer := b/group_size;
+      constant b_position : integer := group_size*group_idx + (b mod group_size);
+    begin
+      if (state < cycles-1) then
+        -- bfly_in_top(b) <= fly_raster(b_position);
+        -- bfly_in_bot(b) <= fly_raster(b_position + b_size);
+
+        mul_raster(b_position)          <= bfly_out_top(b);
+        mul_raster(b_position + b_size) <= bfly_out_bot(b);
+
+        fly_done(b) <= '1';
+      else
+        if (b = 0) then
+          out_buff(n_points/2) <= bfly_in_bot(0); -- Hermitian limit
+        end if;
+        out_buff(b) <= bfly_in_top(b);
+      end if;
+    end process proc_fly;
+  end generate gen_bfly;
+  fly_done_all <= and(fly_done);
+
+
+
+  -- Generate complex constant multiplers and their behavior
   gen_wmul : for k in 0 to n_points/4-2 generate
 
     check_wsym : if (k = n_points/8-1) generate
@@ -152,47 +194,41 @@ begin
           
     end generate check_wsym;
 
+
+
+
+
   end generate gen_wmul;
+  mul_done_all <= and(mul_done);
 
-    buf_raster <= mul_raster;
 
-  -- Generate butterflies and their behavior
-  gen_bfly : for b in 0 to n_points/2-1 generate
 
-    wmul_wsym : component b25_butterfly
-      port map (
-        i_top => bfly_in_top(b),
-        i_bot => bfly_in_bot(b),
-        o_top => bfly_out_top(b),
-        o_bot => bfly_out_bot(b)
-      );
+  proc_loop : process (mul_done_all) is
+    constant b_size     : integer := 2**to_integer(state) + 1;
+    constant group_size : integer := 2**to_integer(state);
+    variable group_idx  : integer;
+    variable b_position : integer;
+  begin
+    if (state < cycles-1) then
+      for b in 0 to n_points-1 loop
+        group_idx  := b/group_size;
+        b_position := group_size*group_idx + (b mod group_size);
 
-    proc_fly : process (state) is
-      constant b_size     : integer := 2**to_integer(state) + 1;
-      constant group_size : integer := 2**to_integer(state);
-      constant group_idx  : integer := b/group_size;
-      constant b_position : integer := group_size*group_idx + (b mod group_size);
-    begin
-      if (state < cycles-1) then
         bfly_in_top(b) <= fly_raster(b_position);
         bfly_in_bot(b) <= fly_raster(b_position + b_size);
+      end loop;
+    else
+      for b in 0 to n_points-1 loop
+        group_idx  := b/group_size;
+        b_position := group_size*group_idx + (b mod group_size);
 
-        mul_raster(b_position)          <= bfly_out_top(b);
-        mul_raster(b_position + b_size) <= bfly_out_bot(b);
-      else
         if (b = 0) then
           out_buff(n_points/2) <= bfly_in_bot(0); -- Hermitian limit
         end if;
         out_buff(b) <= bfly_in_top(b);
-      end if;
-    end process proc_fly;
-  end generate gen_bfly;
+      end loop;
+    end if;
+  end process proc_loop;
   o <= out_buff;
-
-
-
-
-
-
 
 end architecture synth;
