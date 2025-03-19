5d708c088c2f5fa856b9d74aa88aa438d6ceb0d4
RicardoLera <ricardodclera@gmail.com>
2025-03-19 13:50:07 -0300

    found second lsp clash (kill me)

--- src/fft/fft.vhd
+++ src/fft/fft.vhd
@@ -19,16 +19,13 @@ end entity fft;
 
 architecture synth of fft is
 
-  --variable n_muls : natural range 0 to 2056 := 2*(n_points-6)-1;
-
-  constant the_log : integer := integer(ceil(log2(real(n_points))));
-
-  signal state   : integer range 0 to the_log+1 := 0; -- state 1 is synchronous start
+  constant the_log         : integer := integer(ceil(log2(real(n_points))));
+  signal state             : integer range 0 to the_log+1 := 0; -- state 1 is synchronous start
 
   signal in_raster         : b25_real_array(0 to n_points-1) := (others => (others => '0'));
   signal bfly_in, bfly_out : b25_2d_complex_array(0 to n_points/2-1)(0 to 1) := (others => (others => (others => (others => '0')))); -- 0-top; 1-bottom
-  signal wmul_in, wmul_out : b25_3d_complex_array(0 to n_points/4-1)(0 to n_points/4-1)(0 to the_log) := (others => (others => (others => (others => (others => '0')))));
-  signal out_buff, wmul_trigger : b25_complex_array(0 to n_points-1) := (others => (others => (others => '0')));
+  signal wmul_in, wmul_out : b25_3d_complex_array(0 to n_points/4)(0 to n_points/4-1)(1 to the_log-1) := (others => (others => (others => (others => (others => '0')))));
+  signal wmul_trigger, out_buff : b25_complex_array(0 to n_points-1) := (others => (others => (others => '0')));
 
   function fft_scramble_lut(x, y, z : integer) return integer is
     variable idx     : integer := 0;
@@ -51,7 +48,7 @@ architecture synth of fft is
   function bfly_lut(s, n : integer) return integer_pair is
     constant b : integer := (n/(2**s)) * (2**(s-1)) + (n mod (2**(s-1))); -- bfly_pos = bfly_group*group_size + pos_in_group
     -- Yes, (n/(2**s)) * (2**(s-1)) = n/2, except NOT because rounding. Leave it like that, it's synth time
-    constant tb : boolean := (n mod (2**s) < 2**(s-1));
+    constant tb : boolean := not (n mod (2**s) < 2**(s-1));
   begin
     if ((s < 1) or (s > the_log)) then
       return (0,0);
@@ -66,16 +63,15 @@ architecture synth of fft is
 
   -- wmul_lut converts n to the respective w multipliers, in their respective state, or returns (0,0,1) if no multiplication is needed
   function wmul_lut(s, n : integer) return integer_trio is
-    constant valid : boolean := 
-    constant w : integer := 
-    constant m : integer := 
-
+    constant valid : boolean := not (n mod (2**(s+1)) < 2**s); -- yes, it's tb for the next state. I hate/love Fourier symmetry
+    constant w : integer := (n mod (2**s)) * (2**(the_log-s-1)); -- pos_in_group of the next state times decreasing constant (2**(the_log-s-1))
+    constant m : integer := ((n/(2**(s+1))) mod (n_points/(2**(s+1)))); -- n's group index mod number_of_groups (next state)
   begin
-    if ((s < 1) or (s > the_log) or not valid) then
+    if ((s < 1) or (s > the_log-1) or not valid) then
       return (0,0,1);
     else
-      if (w >= n_points/4) then
-        return (w,m,1);
+      if (w > n_points/4) then
+        return (w - (w mod n_points/4),m,1);
       else
         return (w,m,0);
       end if;
@@ -95,7 +91,6 @@ begin
 
   -- Generate butterflies
   gen_bfly : for b in 0 to n_points/2-1 generate
-
     bfly : component b25_butterfly
       port map (
         i_top => bfly_in(b)(0),
@@ -103,13 +98,12 @@ begin
         o_top => bfly_out(b)(0),
         o_bot => bfly_out(b)(1)
       );
-
   end generate gen_bfly;
 
   -- Generate complex constant multiplers
   gen_wmul : for w in 1 to n_points/4-1 generate -- e.g., w1~w7
     gen_wmul2 : for m in 0 to n_points/4-1 generate -- for each same w mul within the same state
-      gen_wmul3 : for r in 0 to the_log generate -- working backwards from last state
+      gen_wmul3 : for r in 1 to the_log-1 generate -- working backwards from last state
         gen_wmul_check : if (w mod (2**r) = 0) generate
 
           wmul : component b25_wmul
@@ -131,11 +125,9 @@ begin
   -- State machine
   state_machine : process (clock) begin
     if rising_edge(clock) then
-
       if (reset) then
         state <= 0;
         s_ready <= '0';
-
       elsif (start) then
         if (state < the_log+1) then
           state <= state + 1;
@@ -143,7 +135,6 @@ begin
           s_ready <= '1';
         end if;
       end if;
-      
     end if;
   end process state_machine;
 
@@ -154,134 +145,63 @@ begin
       proc_bfly : process (state) is
       begin
         for n in 0 to n_points-1 loop -- is n static? Otherwise out_buff is gonna clash
-          if ((b = bfly_lut(state, n)(0)) and (tb = bfly_lut(state, n)(1))) then
+          if (state > 0) then
+            if ((b = bfly_lut(state, n)(0)) and (tb = bfly_lut(state, n)(1))) then
 
-            if (state > 0) then
               if (state < the_log) then
                 if (state = 1) then
                   bfly_in(b)(tb) <= (in_raster(n), 25b"0");
                 else
                   if (wmul_lut(state, n) = (0,0,1)) then -- verify if there is a corresponding multiplier
-                    bfly_in(b)(tb) <= wmul_out(wmul_lut(state-1, n)(0))(wmul_lut(state-1, n)(1))(state);
+                    bfly_in(b)(tb) <= wmul_out(wmul_lut(state, n)(0))(wmul_lut(state, n)(1))(state);
                   else
-                    bfly_in(b)(tb) <= bfly_out(bfly_lut(state-1, n)(0))(bfly_lut(state-1, n)(1));
+                    bfly_in(b)(tb) <= bfly_out(bfly_lut(state, n)(0))(bfly_lut(state, n)(1));
                   end if;
                 end if;
               else
-                if (n < n_points/2) then
-                  out_buff(n) <= bfly_in(n)(0);
+                if (n < n_points/2) then -- top/bottom
+                  out_buff(n) <= bfly_out(n)(0);
                 else
-                  out_buff(n) <= bfly_in(n mod n_points/2)(1);
+                  out_buff(n) <= bfly_out(n mod n_points/2)(1);
                 end if;
               end if;
-            end if;
 
+            end if;
           end if;
         end loop;
       end process proc_bfly;
     end generate gen_procs_bfly_tb;
   end generate gen_procs_bfly;
-  o <= out_buff(0 to 16);
+  o <= out_buff(0 to n_points/2);
 
   gen_procs_wmul : for n in 0 to n_points-1 generate -- wmul_in(w)(m)(state)[b25C] <= bfly_out(b)(tb)[b25C]
-    wmul_trigger(n) <= bfly_out(bfly_lut(state, n)(0))(bfly_lut(state, n)(1));
+    wmul_trigger(n) <= bfly_out(bfly_lut(state, n)(0))(bfly_lut(state, n)(1)) when state > 0  else (others => (others => '0'));
     proc_bfly : process (wmul_trigger(n)) is
       variable b, tb : integer;
     begin
-      b  := bfly_lut(state, n)(0);
-      tb := bfly_lut(state, n)(1);
-      for w in 0 to n_points/4-1 loop
-        for m in 0 to n_points/4-1 loop
-          if ((w = wmul_lut(state, n)(0)) and (m = wmul_lut(state, n)(1)) and (wmul_lut(state, n) /= (0,0,1))) then
+      if ((state > 0) and (state < the_log)) then
+        b  := bfly_lut(state, n)(0);
+        tb := bfly_lut(state, n)(1);
+        for w in 0 to 8 loop -- n_points/4
+          for m in 0 to 7 loop -- n_points/4-1
+            if ((w = wmul_lut(state, n)(0)) and (m = wmul_lut(state, n)(1)) and (wmul_lut(state, n) /= (0,0,1))) then
 
-            if (wmul_lut(state, n)(2) = 1) then -- multiply by i (because, e.g., w1 = i*w9)
-              wmul_in(w)(m)(state) <= (                            -- (a + bi)*i = 
-                (not bfly_out(b)(tb)(1)(24) & bfly_out(b)(tb)(1)), -- -b
-                bfly_out(b)(tb)(0)                                 -- +ai
-              );
-            else
-              wmul_in(w)(m)(state) <= bfly_out(b)(tb);
+              if (wmul_lut(state, n)(2) = 1) then -- multiply by i (because, e.g., w1 = i*w9)
+                report "i   state = " & integer'image(state) & "   n = " & integer'image(n) & "   b = " & integer'image(b) & "   tb = " & integer'image(tb) & "   w = " & integer'image(w) & "   m = " & integer'image(m);
+                wmul_in(w)(m)(state) <= (                                         -- (a + bi)*i = 
+                  (not bfly_out(b)(tb)(1)(24) & bfly_out(b)(tb)(1)(23 downto 0)), -- -b
+                  bfly_out(b)(tb)(0)                                              -- +ai
+                );
+              else
+                report "r   state = " & integer'image(state) & "   n = " & integer'image(n) & "   b = " & integer'image(b) & "   tb = " & integer'image(tb) & "   w = " & integer'image(w) & "   m = " & integer'image(m);
+                wmul_in(w)(m)(1) <= bfly_out(b)(tb); -- lsp clash
+              end if;
+
             end if;
-            
-          end if;
+          end loop;
         end loop;
-      end loop;
+      end if;
     end process proc_bfly;
   end generate gen_procs_wmul;
-
-  -- -- Subcycle 1: Butterflies
-  -- gen_procs_bfly : for n in 0 to n_points-1 generate
-
-  --   proc_bfly : process (state) is
-  --     variable b, tb, w, mn, w_mod : integer;
-  --   begin
-
-  --     report "sc1 trigger";
-
-  --     if (state > 0) then
-  --       b  := bfly_lut(state, n)(0);
-  --       tb := bfly_lut(state, n)(1);
-  --       w  := wmul_lut(state-1, n)(0);
-  --       mn := wmul_lut(state-1, n)(1);
-  --       w_mod := w mod n_points/4;
-
-  --       if (state < the_log) then
-  --         if (state = 1) then
-  --           -- checked via report, all processes make it here
-  --           -- also checked b/tb values are perfect here
-  --           report "n = " & integer'image(n) & "   b = " & integer'image(b) & "   tb = " & integer'image(tb) ;
-  --           bfly_in(b)(tb) <= (in_raster(n), 25b"0");
-  --           report integer'image(to_integer(unsigned(bfly_in(b)(tb)(0))));
-          
-  --           -- last ditch effort: try switching back to std=02
-  --           -- ghdl only partially supports 2008, it might not handle multidimensional fuckery very well
-
-  --         else
-  --           bfly_in(b)(tb) <= wmul_out(w_mod)(mn);
-  --         end if;
-  --       else
-  --         if (n < n_points/2) then
-  --           out_buff(n) <= bfly_in(n)(0);
-  --         else
-  --           out_buff(n) <= bfly_in(n mod n_points/2)(1);
-  --         end if;
-  --       end if;
-        
-  --     end if;
-
-  --   end process proc_bfly;
-  -- end generate gen_procs_bfly;
-  -- o <= out_buff(0 to 16);
-
-  -- -- Subcycle link
-  -- gen_link_b : for b in 0 to n_points/2-1 generate
-  --   process(bfly_out) begin     -- make a fucking latch somehow
-  --     sc_link(b) <= '1';
-  --   end process;
-  -- end generate gen_link_b;
-  -- link_trigger <= or(sc_link);          -- can this fail in board synthesis due to spacing?
-
-  -- -- Subcycle 2: Omega Multipliers
-  -- gen_procs_wmul : for n in 0 to n_points-1 generate
-  --   proc_wmul : process (link_trigger) is
-  --     variable b, tb, w, mn, w_mod : integer := 0;
-  --   begin
-
-  --     b  := bfly_lut(state, n)(0);
-  --     tb := bfly_lut(state, n)(1);
-  --     w  := wmul_lut(state, n);
-  --     w_mod := w mod n_points/4;
-
-  --     if (w >= n_points/4) then -- multiply by i (because, e.g., w1 = i*w9)
-  --       wmul_in(w_mod)(state) <= (                              -- (a + bi)*i = 
-  --         (not bfly_out(b)(tb)(1)(24) & bfly_out(b)(tb)(1)), -- -b
-  --         bfly_out(b)(tb)(0)                                 -- +ai
-  --       );
-  --     else
-  --       wmul_in(w_mod)(state) <= bfly_out(b)(tb);
-  --     end if;
-
-  --   end process proc_wmul;
-  -- end generate gen_procs_wmul;
 
 end architecture synth;
--- src/fft/fft_tb.vhd
+++ src/fft/fft_tb.vhd
@@ -11,74 +11,48 @@ architecture arch of fft_tb is
 
   constant nx, ny, nz : natural range 0 to 16 := 2;
 
-  type test_tuple_t is array(0 to 1) of b25_3d_real_array(0 to nx-1)(0 to ny-1)(0 to nz-1);
-
-  constant test_tuple : test_tuple_t := (
-  ( ( ( "0000000010000000000000000",
+  constant test_arr : b25_3d_real_array(0 to nx-1)(0 to ny-1)(0 to nz-1) := (
+    ( ( "0000000010000000000000000",
         "0000000010000000000000000" ),
       ( "0000000010000000000000000",
         "0000000010000000000000000" ) ),
     ( ( "0000000010000000000000000",
         "0000000010000000000000000" ),
       ( "0000000010000000000000000",
-        "0000000010000000000000000" ) ) ),
-  ( ( ( "0000000010000000000000000",
-        "0000000010000000000000000" ),
-      ( "0000000010000000000000000",
-        "0000000010000000000000000" ) ),
-    ( ( "0000000010000000000000000",
-        "0000000010000000000000000" ),
-      ( "0000000010000000000000000",
-        "0000000010000000000000000" ) ) )
+        "0000000010000000000000000" ) )
   );
 
-  signal   img_in,  ker_in  : b25_3d_real_array(0 to nx-1)(0 to ny-1)(0 to nz-1);
-  signal   img_out, ker_out : b25_complex_array(0 to 16) := (others => (others => (others => '0')));
-  signal   clock, start, reset, s_ready_i, s_ready_k, simulate : std_logic := '0';
-  constant clockperiod : time := 1 ms; -- 1khz
+  signal   i : b25_3d_real_array(0 to nx-1)(0 to ny-1)(0 to nz-1);
+  signal   o : b25_complex_array(0 to 16) := (others => (others => (others => '0')));
+  signal   clock, start, reset, s_ready, simulate : std_logic := '0';
+  constant clockperiod : time := 1 ms;
 
 begin
 
   clock <= (not clock) and simulate after clockperiod / 2;
 
-  -- this is implied
-  -- img_pad <= (0 to 1 => img_in(0 to 1), 3 to 4 => img_in(2 to 3), 9 to 10 => img_in(4 to 5), 12 to 13 => img_in(6 to 7), (others => (others => '0')));
-  -- ker_pad <= (0 to 1 => ker_in(0 to 1), 3 to 4 => ker_in(2 to 3), 9 to 10 => ker_in(4 to 5), 12 to 13 => ker_in(6 to 7), (others => (others => '0')));
-
-  dut0 : component fft
+  dut : component fft
     generic map (2, 2, 2, 32)
     port map (
-      i       => img_in,
-      o       => img_out,
+      i       => i,
+      o       => o,
       clock   => clock,
       reset   => reset,
       start   => start,
-      s_ready => s_ready_i
+      s_ready => s_ready
     );
-
-  -- dut1 : component fft
-  --   generic map (2, 2, 2, 32)
-  --   port map (
-  --     i       => ker_in,
-  --     o       => ker_out,
-  --     clock   => clock,
-  --     start   => start,
-  --     reset   => reset,
-  --     s_ready => s_ready_k
-  --   );
 
   test : process is
   begin
 
     simulate <= '1';
-    img_in <= test_tuple(0);
-    ker_in <= test_tuple(1);
+    i <= test_arr;
     
     reset <= '1';
     wait for 2 ms;
     reset <= '0';
     start <= '1';
-    wait until (s_ready_i = '1') for 50 ms ;
+    wait until (s_ready = '1') for 50 ms ;
     start <= '0';
 
     simulate <= '0';
