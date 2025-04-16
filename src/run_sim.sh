b179982870440fa4408e65621cfb7ade75cdc8f2
RicardoLera <ricardodclera@gmail.com>
2025-04-16 09:32:41 -0300

    Update synth script

--- src/run_sim.sh
+++ src/run_sim.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+
+# Synthesis relies on 3 external softwares: ghdl, yosys and ghdl-yosys-plugin
+# The following are the respective versions of these programs under which the first synthesis test was successful:
+
+# yosys --version
+# Yosys 0.51+162 (git sha1 c261da4e7, g++ 14.2.1 -march=x86-64 -mtune=generic -O2 -fno-plt -fexceptions -fstack-clash-protection -fcf-protection -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -ffile-prefix-map=/home/seth/.cache/yay/yosys-nightly/src=/usr/src/debug/yosys-nightly -flto=auto -fPIC -O3)
+# 
+# pacman -Q yosys
+# yosys-nightly 1:20250409_v0.51_166_gc261da4e7-1
+
+# ghdl --version
+# GHDL 6.0.0-dev (b6.0.0.r62.g57dc78c76) [Dunoon edition]
+#  Compiled with GNAT Version: 14.2.1 20250207
+#  GCC 12.4.0 code generator
+# 
+# pacman -Q ghdl
+# ghdl-gcc-git 6.0.0dev.r10015.g57dc78c76-1
+
+# pacman -Q ghdl-yosys-plugin
+# ghdl-yosys-plugin-git r231.8c29f2c-1
+# 
+# github.com/ghdl/ghdl-yosys-plugin/
+# Exact commit at test time -> 8c29f2cc7cc3b8c979acd02f543d25f321b55c30
+
 ghdl remove
 
 if [ "$1" = "synth" ] ; then
@@ -9,7 +33,8 @@ if [ "$1" = "synth" ] ; then
 
   yosys -m ghdl -p \
     "ghdl --std=08 -fsynopsys --latches $comp_files -e hecate_oa_tb; synth_xilinx -top hecate_oa_tb -family $family -flatten; json -o yosys_out/$family.json" \
-  &> yosys_out/"$family".txt
+  2>&1 | tee >(tail -n 128 > yosys_out/"$family"_synth_log_tail.txt)
+  # &> yosys_out/"$family".txt
 
 else
 
